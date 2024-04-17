5f36d46bd81ea99be470d959d67acaf84d698478
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-04-17 05:44:45 +0000

    Learning Javascript variables

---
+++ 01_basics/01variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "kishor@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+
+// accountId = 2
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "benguluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/* prefer not to use var because of block scope and funtion scope */
