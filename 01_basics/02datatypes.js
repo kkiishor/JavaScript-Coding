3f8e57175337fa61d7758baeb34a8ebca4897d55
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-04-17 17:19:32 +0000

    Learning Javascript Datatypes

---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,12 @@
+"use strict" //use all js code as newer version
+
+//alert("hello");   //we are using node js, not broweser
+
+console.log(3+3);
+console.log("kishor");
+
+console.log(typeof "kishor");
+console.log(typeof null);
+console.log(typeof undefine);
+
+console.log([typeof "kishor", typeof null, typeof undefine]);
