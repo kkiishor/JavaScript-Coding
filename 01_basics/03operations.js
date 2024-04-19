d707adb45b58594ed1d38ab2e104474ab1589e8c
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-04-19 03:29:26 +0000

    ja operations

---
+++ 01_basics/03operations.js
@@ -0,0 +1,13 @@
+let score = 33;
+
+//console.table([10*2, 10+2, 10/2, 9%2]);
+
+let test = 3 + "3";
+console.log(test);
+
+let test1 = "3" + 3;
+console.log(test1);
+
+
+console.log(1 + 1 + "1");
+console.log("1" + 1 + 1);
