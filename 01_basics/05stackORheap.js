1f8e4b2e20f2095ac087b8a200006f1d48b0ce77
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-04-19 04:49:35 +0000

    Learned Stack Memory and Heap memory

---
+++ 01_basics/05stackORheap.js
@@ -0,0 +1,15 @@
+//stack(primitive)
+//heap (non primitive)
+
+let myyutube = "kishor";  //stack
+
+let myobj = {
+    name: "kishir",       //heap
+    age: 22,
+}
+
+/* 
+stack: you get copy of original value in stack
+
+heap: you get reference of original value in heap
+*/
