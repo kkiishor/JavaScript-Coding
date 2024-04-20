00f93124bfae0292200284edc9a85423fa7b383d
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-04-20 04:23:20 +0000

    objects in depth

---
+++ 02_advance/01objects.js
@@ -0,0 +1,18 @@
+// singleton
+// object.creat
+
+// object literals
+
+const jsuser = {
+    name: "kishor",
+    age: 22,
+    location: "nagar",
+    email: "kishor@google.com",
+    isLoggedIn: false,
+    lastLoginDay: ["Monaday", "saturday"]
+
+}
+
+console.log(jsuser.name);
+console.log(jsuser["age"]);
+
