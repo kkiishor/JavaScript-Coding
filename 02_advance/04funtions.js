e67341bdc4006548e068db174be17703b9dc8b28
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-04-21 04:19:02 +0000

    passing object and array funtion

--- 02_advance/04funtions.js
+++ 02_advance/04funtions.js
@@ -7,10 +7,31 @@ function addition(num1, num2, num3){
 
 function userState(userName = "patil"){
     if(userName === undefined){
-        console.log("Please enter name");
+        //console.log("Please enter name");
         return
     }
     return `${userName} just logged in` 
 }
 
-console.log(userState("kishor"));
+//console.log(userState("kishor"));
+
+
+function someone(val1, ...num1){
+    return num1
+}
+
+//console.log(someone(300, 200, 500));
+
+function objecthandle(anyobject){
+ return `name is ${anyobject.username} and id is ${anyobject.id}`
+}
+
+const user = {
+    username: "lishor",
+    id: 200
+}
+
+console.log(objecthandle({
+    username: "Vaibhav",
+    price: 600
+}));
