9cdbebc44faafc9757d357cfc9a81ad1e484386d
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-04-22 04:07:58 +0000

    js code execution

---
+++ 02_advance/jsexecution.js
@@ -0,0 +1,12 @@
+/* javascript execution contex
+ 1. Global execution contex                 reffered to this keyword
+ 2. memory phase/creation phase             memory are allocated to all variable,functions 
+                                            and assigned undefined value
+ 3. execution phase                         values are assign to variable and there is funtion 
+                                            it creates new box for funtion execution 
+                                            1. new variable evironment
+                                            2. execution thred
+                                            3. funtion values are return to global contex 
+
+
+ */
