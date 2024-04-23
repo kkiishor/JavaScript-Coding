b8e3fb4d64c2de3d33a645c6715c654cf0266258
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-04-23 04:46:28 +0000

    foreach, forin, forof

---
+++ 02_advance/07loops.js
@@ -0,0 +1,50 @@
+// for each loop
+
+// const arr = ["kishor", "vaibhav", "satyam", "omkar"]
+
+// arr.forEach( (value, index, arr)=> {
+//     console.log(index, value, arr);
+// })
+
+// const myobj = [
+//     {
+//         name: "kishor",
+//         id: 2,
+//         email: "kishor@gmail"
+//     },
+//     {
+//         name: "vaibhav",
+//         id: 3
+
+//     },
+//     {
+//         name: "satyam",
+//         id: 4
+
+//     }
+
+// ]
+
+// myobj.forEach( (i) => {
+//    // console.log(i.email);
+// })
+
+
+const item = [1 ,2 ,3 ,4, 5]
+const copy = []
+
+item.forEach( (i)=> {
+    copy.push(i);
+})
+
+copy.forEach( (i)=> {
+    console.log(i)
+})
+
+
+// foreach is used for array only
+// for in:  is used for return keys
+// for of: is used for return values
+
+
+
