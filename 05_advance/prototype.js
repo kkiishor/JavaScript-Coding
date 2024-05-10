45c9af07fac6244e2b314d42c04dce1e325c3868
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-05-10 12:08:01 +0000

    prototypes

---
+++ 05_advance/prototype.js
@@ -0,0 +1,18 @@
+// let myname = "kishor     "
+// console.log(myname.trim().length);
+
+
+let myheros = ["thor", "spiderman", "superman"]
+
+let heropower = {
+    thor = "hammer"
+    spiderman = "sling"
+
+    getSpiderPower: function () {
+        console.log('spidy power is ${this.spiderman}');
+    }
+}
+
+
+myheros.hitesh()
+
