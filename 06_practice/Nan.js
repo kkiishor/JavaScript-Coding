bb3f422dfde3666313e7cb8bd1849daf286c3481
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-05-12 18:05:22 +0000

    ...

--- 06_practice/Nan.js
+++ 06_practice/Nan.js
@@ -4,6 +4,22 @@
 // const num = 70
 // console.log(isNaN(num));
 
-(function (){
-    console.log("Kishor");
-}) ()
+// (function (){
+//     console.log("Kishor");
+// }) ()
+
+function allLetter(inputtxt)
+  {
+   var letters = /^[A-Za-z]+$/;
+   if(inputtxt.value.match(letters))
+     {
+      return true;
+     }
+   else
+     {
+     alert("message");
+     return false;
+     }
+  }
+
+  allLetter("The quick brown fox jumps over the lazy dog" )
