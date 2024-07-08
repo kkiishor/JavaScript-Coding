3d8b9b2e793a3c5572626dad1f3f8a241cc35f95
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-07-08 18:00:09 +0000

    Practice

--- 06_practice/binarySearch.js
+++ 06_practice/binarySearch.js
@@ -1,20 +1,24 @@
 
 const array = [1,2,3,5,7,10]
 const value = 5;
+let start = array[0];
+let end = array[array.length];
 
-function binarySearch(first, last) {
-    const mid = (array[0]+array[array.length-1])/2
-    if(mid == value) return;
-    if(mid<value){
-        first = mid+1;
-        last = array.length-1;
-        return binarySearch(first , last)
-    }
-    if (mid>value){
-        first= array[0]
-        last= mid-1;
-        return binarySearch(first, last)
-    } 
+console.log(end);
 
-}
-console.log(binarySearch());
+// function binarySearch(array, value, start, end) {
+//     const mid = (array[0]+array[array.length-1])/2
+//     if(mid == value) return;
+//     if(mid<value){
+//         first = mid+1;
+//         last = array.length-1;
+//         return binarySearch(first , last)
+//     }
+//     if (mid>value){
+//         first= array[0]
+//         last= mid-1;
+//         return binarySearch(first, last)
+//     } 
+
+// }
+// console.log(binarySearch());
