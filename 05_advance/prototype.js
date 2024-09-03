ec268e633aec897f6db03126d8751f16f0d91cfa
Kishor <86660017+kkiishor@users.noreply.github.com>
2024-09-03 04:00:30 +0000

    Updated

--- 05_advance/prototype.js
+++ 05_advance/prototype.js
@@ -5,14 +5,29 @@
 let myheros = ["thor", "spiderman", "superman"]
 
 let heropower = {
-    thor = "hammer"
-    spiderman = "sling"
+    thor: "hammer",
+    spiderman : "sling",
 
     getSpiderPower: function () {
-        console.log('spidy power is ${this.spiderman}');
+       // console.log('spidy power is ${this.spiderman}');
     }
 }
+// myheros.hitesh()
 
 
-myheros.hitesh()
+
+const teacher = {
+    makeVideo: true
+}
+const supportMan = {
+    isAvailable: false
+}
+const TAsupport={
+    makeAssi: "js assi",
+    fulltime: true,
+    // __proto__: supportMan
+}
+
+TAsupport.__proto__ = supportMan
+console.log(TAsupport);
 
